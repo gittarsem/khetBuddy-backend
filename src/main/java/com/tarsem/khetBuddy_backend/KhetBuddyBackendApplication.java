7d18e5b8c3f080e9039941fd2f7c89b1297e26b4
gittarsem <work4tarsemgulab@gmail.com>
2026-02-12 17:15:17 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/tarsem/khetBuddy_backend/KhetBuddyBackendApplication.java
@@ -0,0 +1,14 @@
+package com.tarsem.khetBuddy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhetBuddyBackendApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Im Running... ");
+		SpringApplication.run(KhetBuddyBackendApplication.class, args);
+	}
+
+}
