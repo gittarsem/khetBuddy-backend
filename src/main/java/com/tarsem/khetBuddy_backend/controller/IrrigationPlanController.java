ced9205c8d5238903e2bc09c9dce776d6b5cb8f9
gittarsem <work4tarsemgulab@gmail.com>
2026-04-07 15:28:09 +0530

    chore: add basic Swagger annotations to irrigation controller

--- src/main/java/com/tarsem/khetBuddy_backend/controller/IrrigationPlanController.java
+++ src/main/java/com/tarsem/khetBuddy_backend/controller/IrrigationPlanController.java
@@ -5,6 +5,11 @@ import com.tarsem.khetBuddy_backend.dto.irrigation.IrrigationAdviceDTO;
 import com.tarsem.khetBuddy_backend.dto.irrigation.IrrigationPlanRequestDTO;
 import com.tarsem.khetBuddy_backend.service.Interfaces.FarmerService;
 import com.tarsem.khetBuddy_backend.service.Interfaces.IrrigationPlanService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequestMapping("/irrigation")
+@Tag(name = "Irrigation Planning", description = "APIs for irrigation advice and scheduling")
 public class IrrigationPlanController {
 
     @Autowired
@@ -21,19 +27,31 @@ public class IrrigationPlanController {
     @Autowired
     public FarmerService farmerService;
 
+    @Operation(
+            summary = "Get Immediate Irrigation Plan",
+            description = "Returns real-time irrigation advice based on farm conditions"
+    )
     @PostMapping("/immediate/{farmId}")
     public ResponseEntity<IrrigationAdviceDTO> getImmediatePlan(
+            @Parameter(description = "Unique ID of the farm", example = "1")
             @PathVariable Long farmId,
+
             @RequestBody IrrigationPlanRequestDTO requestDTO
     ){
-        return ResponseEntity.ok(irrigationService.getImmediatePlan(farmId,requestDTO));
+        return ResponseEntity.ok(irrigationService.getImmediatePlan(farmId, requestDTO));
     }
 
+    @Operation(
+            summary = "Generate Irrigation Schedule",
+            description = "Creates a future irrigation schedule for the farmer"
+    )
     @PostMapping("/schedule/{farmId}")
     public ResponseEntity<FarmerScheduleResponse> generateSchedule(
+            @Parameter(description = "Unique ID of the farm", example = "1")
             @PathVariable Long farmId,
+
             @RequestBody IrrigationPlanRequestDTO requestDTO
     ){
-        return ResponseEntity.ok(irrigationService.generateSchedule(farmId,requestDTO));
+        return ResponseEntity.ok(irrigationService.generateSchedule(farmId, requestDTO));
     }
 }
